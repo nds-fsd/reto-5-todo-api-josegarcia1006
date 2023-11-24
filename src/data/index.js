5aa29c668e88d62b979e49c6d249461ee21e035e
Jose Garcia <josegarcia1006@gmail.com>
2023-11-24 15:50:50 +0100

    Reto 5

--- src/data/index.js
+++ src/data/index.js
@@ -1,16 +1,75 @@
-exports.todos = [{
-  id: 0,
-  text: "Hacer la cama",
-  fecha: new Date("2022-05-21"),
-  done: false
-},
-{ 
-  id: 1,
-  text: "Ir al super",
-  fecha: new Date("2022-05-14"),
-  done: true
-}];
-
-
-
-
+exports.todos = [
+  {
+    id: 0,
+    text: 'Clean rooms and change bed sheets',
+    date: '20/11/2023',
+    done: false,
+  },
+  {
+    id: 1,
+    text: 'Clean bathrooms',
+    date: '21/11/2023',
+    done: true,
+  },
+  {
+    id: 2,
+    text: 'Clean windows',
+    date: '22/11/2023',
+    done: false,
+  },
+  {
+    id: 3,
+    text: 'Deep clean the kitchen',
+    date: '23/11/2023',
+    done: true,
+  },
+  {
+    id: 4,
+    text: 'Vacuum and mop the floors',
+    date: '24/11/2023',
+    done: false,
+  },
+  {
+    id: 5,
+    text: 'Do the laundry',
+    date: '25/11/2023',
+    done: true,
+  },
+  {
+    id: 6,
+    text: 'Dust surfaces',
+    date: '26/11/2023',
+    done: false,
+  },
+  {
+    id: 7,
+    text: 'Organize the entire house',
+    date: '26/11/2023 17:30',
+    done: true,
+  },
+  {
+    id: 8,
+    text: 'Organize closet',
+    date: '25/11/2023',
+    done: false,
+  },
+    {
+      id: 9,
+      text: 'Clean the garage',
+      date: '26/11/2023',
+      done: true,
+    },
+    {
+      id: 10,
+      text: 'Clean oven and Barbeque',
+      date: '24/11/2023',
+      done: false,
+    },
+    {
+      id: 11,
+      text: 'Take Garbage out every day',
+      date: '23/11/2023',
+      done: false,
+    },
+  ];
+  
